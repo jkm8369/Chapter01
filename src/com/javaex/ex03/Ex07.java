0f0deeff966f729897f0654bc306ea017fda5ea3
jkm8369 <jkm8369@naver.com>
2025-04-18 15:36:11 +0900

    2025-04-18 15:36

--- src/com/javaex/ex03/Ex07.java
+++ src/com/javaex/ex03/Ex07.java
@@ -11,6 +11,8 @@ public class Ex07 {
 		System.out.print("점수를 입력하세요 : ");
 		int score = sc.nextInt();
 		
+		
+		/*
 		if(score>=90) {
 			System.out.println("A등급");
 			
@@ -23,10 +25,29 @@ public class Ex07 {
 		} else if(60<=score && score<70) {
 			System.out.println("D등급");
 			
+		} else if(score<60) {
+			System.out.println("F등급");
+			
+		}
+		*/
+		
+		
+		if(score>=90) {
+			System.out.println("A등급");
+			
+		} else if(score>=80) {
+			System.out.println("B등급");
+			
+		} else if(score>=70) {
+			System.out.println("C등급");
+			
+		} else if(score>=60) {
+			System.out.println("D등급");
+			
 		} else {
 			System.out.println("F등급");
+			
 		}
-		
 		
 		
 		sc.close();
