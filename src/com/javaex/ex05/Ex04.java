67d1f261333b2e705f3429c75267b5a95849b672
jkm8369 <jkm8369@naver.com>
2025-04-25 10:00:17 +0900

    2025-04-25 10:00

--- src/com/javaex/ex05/Ex04.java
+++ src/com/javaex/ex05/Ex04.java
@@ -4,27 +4,17 @@ public class Ex04 {
 
 	public static void main(String[] args) {
 		
-		int[] lottos = new int[6];
+		int intArray[] = new int[6];
 		
-		lottos[0] = (int)(Math.random()*45)+1;
-		lottos[1] = (int)(Math.random()*45)+1;
-		lottos[2] = (int)(Math.random()*45)+1;
-		lottos[3] = (int)(Math.random()*45)+1;
-		lottos[4] = (int)(Math.random()*45)+1;
-		lottos[5] = (int)(Math.random()*45)+1;
+		intArray[0] = 3;
+		intArray[1] = 6;
+		intArray[2] = 9;
 		
-		System.out.println(lottos[0]);
-		System.out.println(lottos[1]);
-		System.out.println(lottos[2]);
-		System.out.println(lottos[3]);
-		System.out.println(lottos[4]);
-		System.out.println(lottos[5]);
+		for(int i=0; i<3; i++) {
+			System.out.println(intArray[i]);
+		}
 		
 		
 		
-		
-		
-
 	}
-
 }
